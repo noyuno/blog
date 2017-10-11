6aef5405594705af33c234ed5f3c4d44719fe49c
noyuno <noyuno@users.noreply.github.com>
2017-10-11 16:40:00 +0900

    Update

---
+++ src/301.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+for i in $(find _posts/$1 -type f); do
+    f=$(echo $i | cut -d '/' -f 3 | cut -d '.' -f 1)
+    m=$(echo $f | cut -d '-' -f 2)
+    d=$(echo $f | cut -d '-' -f 3)
+    n=$(echo $f | cut -d '-' -f 4)
+    mkdir -p $1/$m/$d/$n
+    cat << EOF > $1/$m/$d/$n/index.html
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0;url=/blog/$1-$m-$d-$n">
+    </head>
+<body>
+    301 Moved Permanently<br>
+    <a hreF="/blog/$1-$m-$d-$n">/blog/$1-$m-$d-$n</a>
+</body>
+</html>
+EOF
+done
+
