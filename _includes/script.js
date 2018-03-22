d0564e9c4f7a1acb2b3fa477be4329d2b3f374c2
noyuno <noyuno@users.noreply.github.com>
2018-03-22 16:06:08 +0900

    Update

--- _includes/script.js
+++ _includes/script.js
@@ -2,9 +2,30 @@ var white = "#eee";
 var black = "#111";
 
 var color = function (b, c) {
+    var tag = function (tag, set, color) {
+        [].forEach.call(document.getElementsByTagName(tag), function(t) {
+            t.style.border = "1px " + set + " " +color;
+        });
+    };
+    var tagBottom = function (tag, set, color) {
+        [].forEach.call(document.getElementsByTagName(tag), function(t) {
+            t.style.borderBottom = "1px " + set + " " + color;
+        });
+    };
+    var classBottom = function (cla, set, color) {
+        [].forEach.call(document.getElementsByClassName(cla), function(t) {
+            t.style.borderBottom = "1px " + set + " " + color;
+        });
+    };
     if (b) {
         document.body.style.backgroundColor = black;
         document.body.style.color = white;
+        tag("table", "outset", white);
+        tag("th", "inset", white);
+        tag("td", "inset", white);
+        classBottom("masthead", "solid", white);
+        tagBottom("hr", "solid", white);
+        /*
         [].forEach.call(document.getElementsByTagName("table"), function(t) {
             t.style.border = "1px outset " + white;
             var scroll = document.createElement("div");
@@ -24,12 +45,19 @@ var color = function (b, c) {
         [].forEach.call(document.getElementsByTagName("hr"), function(t) {
             t.style.borderBottom = "1px solid" + white;
         });
+        */
         if (c) {
             window.localStorage.setItem("color", "black");
         }
     } else {
         document.body.style.backgroundColor = white;
         document.body.style.color = black;
+        tag("table", "outset", black);
+        tag("th", "inset", black);
+        tag("td", "inset", black);
+        classBottom("masthead", "solid", black);
+        tagBottom("hr", "solid", black);
+        
         [].forEach.call(document.getElementsByTagName("table"), function(t) {
             t.style.border = "1px outset " + black;
         });
@@ -45,6 +73,7 @@ var color = function (b, c) {
         [].forEach.call(document.getElementsByTagName("hr"), function(t) {
             t.style.borderBottom = "1px solid" + black;
         });
+        
         if (c) {
             window.localStorage.setItem("color", "white");
         }
